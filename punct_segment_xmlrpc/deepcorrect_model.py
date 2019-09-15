f99e0b73d57c2bf82dd36cd5547e4c44762c2d6e
David Zhuang <cnbeining@gmail.com>
2019-09-15 02:40:35 -0400

    Models for deepcorrect

---
+++ punct_segment_xmlrpc/deepcorrect_model.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#coding:utf-8
+from txt2txt109 import build_model, infer
+
+
+class DeepCorrect():
+    deepcorrect_model = None
+    
+    def __init__(self, params_path, checkpoint_path):
+        # loading the model
+        DeepCorrect.deepcorrect_model = build_model(params_path)
+        DeepCorrect.deepcorrect_model[0].load_weights(checkpoint_path)
+    
+    def correct(self, sentence, beam_size = 1):
+        if not DeepCorrect.deepcorrect_model:
+            print('Please load the model first')
+        
+        sentence = sentence.strip()
+        sentence = infer(sentence, DeepCorrect.deepcorrect_model[0], DeepCorrect.deepcorrect_model[1],
+                         beam_size = beam_size)
+        
+        return sentence
+
